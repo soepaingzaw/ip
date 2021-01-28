a100e4b34466cd4bbbcf7ea7fc27c023b5c4a0d5
soepaingzaw <soepaing6c2@gmail.com>
2021-01-29 02:48:41 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,28 +27,34 @@ public class Duke {
 
         String arr[]= new String[100];
 
+        String checks[] = new String[100];
+
         int j=0;
 
         while(true) {
-
-
             arr[j] = scan.nextLine();
+            checks[j] = "[✘]";
 
-            if (arr[j].equals("bye")) {
+            String[] parts = arr[j].split(" ");
+
+            if(parts[0].equals("done")){
+                System.out.println("Nice! I've marked this task as done:");
+                checks[Integer.parseInt(parts[1])-1] = "[✓]";
+                System.out.println("[✓] "+ arr[Integer.parseInt(parts[1])-1]);
+            }
+            else if (arr[j].equals("bye")) {
                 System.out.println("BYE! See you again!");
                 break;
             }
-
             else if(arr[j].equals("list")) {
                 int a = j;
                 int b = 0;
                 while (a != 0) {
-                    System.out.println(b + 1 + ". " + arr[b++]);
+                    System.out.println(b + 1 + ". " + checks[b] + " " + arr[b]);
                     a--;
+                    b++;
                 }
-
             }
-
             else {
                 for (int i = 0; i < 85; i++) {
                     System.out.print("_");
