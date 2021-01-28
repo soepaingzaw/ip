6cc506b4324f853a80850a41cc3e522599c3fcb1
soepaingzaw <soepaing6c2@gmail.com>
2021-01-29 01:07:16 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,60 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        for (int i = 0; i < 85; i++) {
+            System.out.print("_");
+
+        }
+        System.out.print("\n");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I  do for you?");
+        for (int i = 0; i < 85; i++) {
+            System.out.print("_");
+
+        }
+        System.out.print("\n");
+
+        Scanner scan = new Scanner(System.in);
+
+        String arr[]= new String[100];
+
+        int j=0;
+
+        while(true) {
+
+
+            arr[j] = scan.nextLine();
+
+            if (arr[j].equals("bye")) {
+                System.out.println("BYE! See you again!");
+                break;
+            }
+            /*
+            else if(arr[j].equals("list")) {
+                int a = j;
+                int b = 0;
+                while (a != 0) {
+                    System.out.println(b + 1 + ". " + arr[b++]);
+                    a--;
+                }
+
+            }
+
+             */
+            else {
+                for (int i = 0; i < 85; i++) {
+                    System.out.print("_");
+                }
+                System.out.print("\n");
+
+                System.out.println(arr[j++]);
+                //System.out.println("added:" + arr[j++]);
+                for (int i = 0; i < 85; i++) {
+                    System.out.print("_");
+                }
+                System.out.print("\n");
+            }
+        }
     }
 }
