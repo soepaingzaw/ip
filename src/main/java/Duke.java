67fece290beacf701aabeb197d6b1649bd10c696
soepaingzaw <soepaing6c2@gmail.com>
2021-02-23 14:10:48 +0800

    before implementation of switch case statements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Duke {
 
     static final int LINES = 85;
+    static final int INPUTS = 100;
+
+    public static Task[] tasks = new Task[INPUTS];
 
     public static void main(String[] args) {
 
         printWelcomeMessage();
         printDivider();
-
-
+        greetUserForTask();
+        receiveInputs();
 
     }
 
@@ -21,8 +24,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("Hello from\n" + logo);
-
+        System.out.print("Hello from\n" + logo);
 
     }
 
@@ -30,9 +32,49 @@ public class Duke {
         for (int i = 0; i < LINES; i++) {
             System.out.print("_");
         }
+        System.out.print("\n");
 
     }
 
+    public static void greetUserForTask() {
+        System.out.print("Hello! I'm Duke\n");
+        System.out.print("What can I do for you?\n");
 
+    }
 
+    public static void receiveInputs() {
+
+        int index = 0;
+        int done = 0;
+
+        while (true) {
+
+            Scanner scan = new Scanner(System.in);
+            String line = scan.nextLine();
+
+            if (line.equals("bye")) {
+                System.out.print("Bye. Hope to see you again soon!\n");
+                break;
+            } else if (line.startsWith("done")) {
+                done = Integer.parseInt(line.substring(5));
+                tasks[done-1].markAsDone();
+                System.out.print("Nice! I've marked this task as done:\n" +
+                        tasks[done-1].print());
+
+            } else if (line.equals("list")) {
+                System.out.print("Here are the tasks in your list:\n");
+                for (int i = 0; i < index; i++) {
+                    System.out.printf("%d." + tasks[i].print(),i+1);
+                }
+            } else {
+
+                tasks[index++] = new Task(line);
+            }
+
+        }
+
+    }
 }
+
+
+
